3168675057cd894f356f0f7b9cf5f16fb9223404
Alex Alegre <alexander.alegre@outlook.com>
2019-11-15 08:52:56 -0700

    remove files that are not needed

--- api/solutions/04-CRUD/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-const Query = require('./query');
-const Mutation = require('./mutation');
-const { GraphQLDateTime } = require('graphql-iso-date');
-
-module.exports = {
-  Query,
-  Mutation,
-  DateTime: GraphQLDateTime
-};
